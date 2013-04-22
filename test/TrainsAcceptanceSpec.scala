487100068d21219b21055aba998fcef769be1101
Sergey Moshnikov <s.moshnikov@gmail.com>
2013-04-22 08:23:45 +0400

    First acceptance tests

---
+++ test/TrainsAcceptanceSpec.scala
@@ -0,0 +1,8 @@
+import org.scalatest.FunSpec
+
+class TrainsAcceptanceSpec extends FunSpec {
+  describe("initial configurations") {
+    it("reports crash when 2 trains start from the same station")(pending)
+    it("reports success when 2 trains start from different stations")(pending)
+  }
+}
