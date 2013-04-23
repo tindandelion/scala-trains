6b79d98490fd9accce853c1e32a07fcecc2aa1fe
Sergey Moshnikov <s.moshnikov@gmail.com>
2013-04-23 09:45:45 +0400

    Some more collisions

--- src/org/dandelion/trains/Train.scala
+++ src/org/dandelion/trains/Train.scala
@@ -1,13 +1,8 @@
 package org.dandelion.trains
 
 class Train(val route: List[String]) {
-
-  def startStation = route.head
-  def finishStation = route.last
+  def collidesWith(others: List[Train]): Boolean = others.exists(collidesWith)
 
   def collidesWith(other: Train): Boolean =
-    (other.startStation == this.startStation) ||
-      (other.finishStation == this.finishStation)
-
-  def collidesWith(others: List[Train]): Boolean = others.exists(collidesWith)
+    (other.route, this.route).zipped.exists(_ == _)
 }
--- test/TrainSpec.scala
+++ test/TrainSpec.scala
@@ -4,13 +4,34 @@ import org.scalatest.FunSpec
 class TrainSpec extends FunSpec {
   it("collides with other train if have same start station") {
     val me = new Train(List("a", "b"))
-    assert(me.collidesWith(new Train(List("a", "c"))), "collision is expected")
+    assert(me.collidesWith(new Train(List("a", "c"))), "collision is expected at the station a")
     assert(!me.collidesWith(new Train(List("c", "d"))), "collision is not expected")
   }
 
-  it("collides with other train if have same finish station") {
+  it("collides with other train if have same final station") {
     val me = new Train(List("a", "b"))
-    assert(me.collidesWith(new Train(List("c", "b"))), "collision is expected")
+    assert(me.collidesWith(new Train(List("c", "b"))), "collision is expected at the station b")
     assert(!me.collidesWith(new Train(List("c", "d"))), "collision is not expected")
   }
+
+  it("collides with other train at the same intermediate station") {
+    val me = new Train(List("a", "b", "c"))
+    val other = new Train(List("c", "b", "a"))
+    assert(me.collidesWith(other), "collision is expected at the station b")
+  }
+
+  it("does not collide with other train if stay at the same station at different time") {
+    val me = new Train(List("a", "b", "c"))
+    val other = new Train(List("c", "d", "b"))
+    assert(!me.collidesWith(other), "collision is not expected")
+  }
+
+  it("collides at the track") {
+    val me = new Train(List("a", "b"))
+    val other = new Train(List("b", "a"))
+
+    assert(me.collidesWith(other), "collision is expected at the track a <-> b")
+  }
+
+
 }
