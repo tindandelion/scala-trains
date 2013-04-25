515ec27c40c8bfbfd54d8825a83577b49789c23d
Sergey Moshnikov <s.moshnikov@gmail.com>
2013-04-25 07:55:13 +0400

    Extracted Trajectory.intersect method

--- src/org/dandelion/trains/Train.scala
+++ src/org/dandelion/trains/Train.scala
@@ -1,13 +1,10 @@
 package org.dandelion.trains
 
-
-
 class Train(route: List[Any]) {
 
   val trajectory = Trajectory.build(route)
 
   def collidesWith(others: List[Train]): Boolean = others.exists(collidesWith)
 
-  def collidesWith(other: Train): Boolean =
-    (other.trajectory, this.trajectory).zipped.exists(_ intersects _)
+  def collidesWith(other: Train): Boolean = Trajectory.intersect(this.trajectory, other.trajectory)
 }
--- src/org/dandelion/trains/Trajectory.scala
+++ src/org/dandelion/trains/Trajectory.scala
@@ -17,9 +17,13 @@ case class AtTrack(fromStation: Any, toStation: Any) extends Position {
 }
 
 object Trajectory {
-  def build(route: List[Any]): List[Position] = build(route, List())
+  type Trajectory = List[Position]
 
-  private def build(route: List[Any], res: List[Position]): List[Position] = {
+  def build(route: List[Any]): Trajectory = build(route, List())
+  def intersect(t1: Trajectory, t2: Trajectory) =
+    (t1, t2).zipped.exists(_ intersects _)
+
+  private def build(route: List[Any], res: Trajectory): Trajectory = {
     route match {
       case Nil => res
       case List(s) => res ++ List(AtStation(s))
