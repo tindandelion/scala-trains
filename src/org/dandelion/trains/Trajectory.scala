8fb311b71a5828a628b368b99493c63d68dd410e
Sergey Moshnikov <s.moshnikov@gmail.com>
2013-04-25 09:30:36 +0400

    Extracted class Trajectory

--- src/org/dandelion/trains/Trajectory.scala
+++ src/org/dandelion/trains/Trajectory.scala
@@ -3,12 +3,14 @@ package org.dandelion.trains
 trait Position {
   def intersects(other: Position): Boolean
 }
+
 case class AtStation(station: Any) extends Position {
   override def intersects(other: Position): Boolean = other match {
     case AtStation(otherStation) => (station == otherStation)
     case _ => false
   }
 }
+
 case class AtTrack(fromStation: Any, toStation: Any) extends Position {
   override def intersects(other: Position): Boolean = other match {
     case AtTrack(fromOther, toOther) =>
@@ -17,21 +19,30 @@ case class AtTrack(fromStation: Any, toStation: Any) extends Position {
   }
 }
 
-object Trajectory {
-  type Trajectory = List[Position]
+class Trajectory(val positions: List[Position]) {
+  def current = positions.head
 
-  def build(route: List[Any]): Trajectory = build(route, List())
-  def intersect(me: Trajectory, other: Trajectory): Boolean = (me, other) match {
-    case (_, Nil) => false
-    case (Nil, _) => false
-    case (List(myPos), otherPos :: otherTail) => myPos.intersects(otherPos) || intersect(me, otherTail)
-    case (myPos :: myTail, List(otherPos)) => myPos.intersects(otherPos) || intersect(myTail, other)
-    case (pos1 :: tail1, pos2 :: tail2) => pos1.intersects(pos2) || intersect(tail1, tail2)
-    case _ => throw new Error("Can not happen")
+  def isFinished = positions.tail.isEmpty
+
+  def rest =
+    if (isFinished) new Trajectory(positions)
+    else new Trajectory(positions.tail)
+
+  def intersects(other: Trajectory): Boolean = {
+    if (current.intersects(other.current)) true
+    else if (isFinished && other.isFinished) false
+    else rest.intersects(other.rest)
   }
 
+}
 
-  private def build(route: List[Any], res: Trajectory): Trajectory = {
+object Trajectory {
+  def build(route: List[Any]): List[Position] = build(route, List())
+
+  def intersect(me: List[Position], other: List[Position]): Boolean =
+    new Trajectory(me).intersects(new Trajectory(other))
+
+  private def build(route: List[Any], res: List[Position]): List[Position] = {
     route match {
       case Nil => res
       case List(s) => res ++ List(AtStation(s))
