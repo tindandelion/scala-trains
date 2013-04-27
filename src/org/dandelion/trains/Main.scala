19571a95aca43874c9511e600d9c3e3f1275dc9c
Sergey Moshnikov <s.moshnikov@gmail.com>
2013-04-27 18:38:31 +0400

    Optimizing Main

--- src/org/dandelion/trains/Main.scala
+++ src/org/dandelion/trains/Main.scala
@@ -7,23 +7,12 @@ case class Collision(_1: Train, _2: Train, position: Position)
 
 
 object Main {
+  def detectCollision(trains: List[Train]): Option[Collision] = {
+    val collisions = for (List(a, b) <- trains.combinations(2);
+         collisionPosition = a.trajectory.intersection(b.trajectory)
+         if (collisionPosition != None)) yield Collision(a, b, collisionPosition.get)
 
-  def detectCollision(trains: List[Train]): Option[Collision] =
-    if (trains.isEmpty) None
-    else {
-      detectCollision(trains.head, trains.tail) match {
-        case None => detectCollision(trains.tail)
-        case found => found
-      }
-    }
-
-  private def detectCollision(_this: Train, those: List[Train]): Option[Collision] =
-    if (those.isEmpty) None
-    else {
-      val that = those.head
-      _this.trajectory.intersection(that.trajectory) match {
-        case Some(position) => Some(Collision(_this, that, position))
-        case _ => detectCollision(_this, those.tail)
-      }
-    }
+    if (collisions.isEmpty) None
+    else Some(collisions.next())
+  }
 }
